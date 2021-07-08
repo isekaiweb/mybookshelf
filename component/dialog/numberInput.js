9b1096e18542b4bd195534df0fe723eff590bb21
isekaiweb <cobasaja271@gmail.com>
2021-07-08 08:15:40 +0700

    move class number input

---
+++ component/dialog/numberInput.js
@@ -0,0 +1,41 @@
+class NumberInput {
+  #value = '';
+  #yearBook = document.querySelector('#info_book > input:last-of-type');
+  #currentYears = new Date().getFullYear();
+
+  constructor(element = this.#yearBook) {
+    this.element = element;
+  }
+
+  #popValue() {
+    this.#value = [...this.#value];
+    this.#value.pop();
+    this.#value = this.#value.join('');
+  }
+
+  #cekType(e) {
+    if (this.element === this.#yearBook) {
+      if (this.#value * 1 > this.#currentYears) {
+        this.#popValue();
+        e.preventDefault();
+      }
+    } else return;
+  }
+
+  mustBeNumber() {
+    this.element.addEventListener('keydown', (e) => {
+      if (e.key < '0' || (e.key > '9' && e.key != 'Backspace'))
+        e.preventDefault();
+      else {
+        if (e.key != 'Backspace') {
+          this.#value += e.key;
+          this.#cekType(e);
+        } else {
+          this.#popValue();
+        }
+      }
+    });
+  }
+}
+
+export default NumberInput;
