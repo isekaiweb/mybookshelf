c0362914c132dff16a0365f55f034b9f16fa1ff2
isekaiweb <cobasaja271@gmail.com>
2021-07-13 14:18:46 +0700

    messure screen

--- util/script.js
+++ util/script.js
@@ -1,6 +1,8 @@
 import { setHeader, setOverflowChip } from '../component/header/script.js';
 import setupLoading from '../component/loading/script.js';
 
+alert(`${window.innerWidth}x${window.innerHeight}`);
+
 setupLoading();
 setHeader();
 setOverflowChip();
