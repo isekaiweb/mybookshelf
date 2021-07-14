13643344781228acb70736e3c2d5ce418181f58e
isekaiweb <cobasaja271@gmail.com>
2021-07-14 20:39:18 +0700

    test

--- component/dialog/numberInput.js
+++ component/dialog/numberInput.js
@@ -25,7 +25,7 @@ class NumberInput {
     this.#value = this.element.value;
     this.element.addEventListener('keydown', (e) => {
       if (detectMob()) {
-        this.element.value = this.element.value.replace(/\D+/g, '');
+        alert(e.code);
       } else {
         if (
           (e.key < '0' || e.key > '9') &&
